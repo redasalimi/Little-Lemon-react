ffe81f133c95fc3de96ed0382d8028e4787a87a2
reda-salimi <salimireda16@gmail.com>
2025-09-03 18:43:41 +0100

    Adding assets and components and meta tags

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import './App.css';
+import Footer from './components/Footer';
+import Header from './components/Header';
+import Main from './components/Main';
+import Nav from './components/Nav';
+
+function App() {
+  return (
+    <>
+      <Header />
+      <Nav />
+      <Main />
+      <Footer />
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Header() {
+  return (
+    <header>
+        <img src={require('../assets/imgs/Logo1.jpg')} alt="Logo" />
+    </header>
+  )
+}
+
+export default Header
